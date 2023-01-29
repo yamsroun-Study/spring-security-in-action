cedda9fe427ca754272837f28bd30df8b6c41455
yamsroun <yamsroun@gmail.com>
2023-01-29 21:36:40 +0900

    5.1

---
+++ src/main/java/yamsroun/ssiach5/config/SecurityConfig.java
@@ -0,0 +1,9 @@
+package yamsroun.ssiach5.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+}
---
+++ src/test/java/yamsroun/ssiach5/SsiaCh5ApplicationTests.java
@@ -0,0 +1,13 @@
+package yamsroun.ssiach5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsiaCh5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
