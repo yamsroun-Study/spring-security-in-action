151731debf0355cfee1eafa52876978f9d9bbafb
yamsroun <yamsroun@gmail.com>
2023-01-29 23:16:31 +0900

    5.2

--- src/main/java/yamsroun/ssiach5/config/SecurityConfig.java
+++ src/main/java/yamsroun/ssiach5/config/SecurityConfig.java
@@ -1,9 +1,18 @@
 package yamsroun.ssiach5.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+    @Bean
+    public InitializingBean initializingBean() {
+        return () -> SecurityContextHolder.setStrategyName(
+            SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
+    }
 }
--- src/main/java/yamsroun/ssiach5/controller/HelloController.java
+++ src/main/java/yamsroun/ssiach5/controller/HelloController.java
@@ -1,13 +1,68 @@
 package yamsroun.ssiach5.controller;
 
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.security.concurrent.DelegatingSecurityContextCallable;
+import org.springframework.security.concurrent.DelegatingSecurityContextExecutorService;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.*;
 
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello!";
+        SecurityContext context = SecurityContextHolder.getContext();
+        Authentication auth = context.getAuthentication();
+
+        return "Hello, " + auth.getName() + "!";
+    }
+
+    @GetMapping("/hello2")
+    public String hello(Authentication auth) {
+        return "Hello, " + auth.getName() + "!";
+    }
+
+    @Async
+    @GetMapping("/bye")
+    public void goodbye() {
+        SecurityContext context = SecurityContextHolder.getContext();
+        String username = context.getAuthentication().getName();
+    }
+
+    @GetMapping("/ciao")
+    public String ciao() throws Exception {
+        Callable<String> task = () -> {
+            SecurityContext context = SecurityContextHolder.getContext();
+            return context.getAuthentication().getName();
+        };
+
+        ExecutorService executor = Executors.newCachedThreadPool();
+        try {
+            var contextTask = new DelegatingSecurityContextCallable<>(task);
+            return "Ciao, " + executor.submit(contextTask).get() + "!";
+        } finally {
+            executor.shutdown();
+        }
+    }
+
+    @GetMapping("/hola")
+    public String hola() throws Exception {
+        Callable<String> task = () -> {
+            SecurityContext context = SecurityContextHolder.getContext();
+            return context.getAuthentication().getName();
+        };
+
+        ExecutorService executor = Executors.newCachedThreadPool();
+        executor = new DelegatingSecurityContextExecutorService(executor);
+        try {
+            return "Hola, " + executor.submit(task).get() + "!";
+        } finally {
+            executor.shutdown();
+        }
     }
 }
