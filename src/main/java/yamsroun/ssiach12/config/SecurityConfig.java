0e39cb3e13b6bafbe4246eb0a25d830bd639a9ca
yamsroun <yamsroun@gmail.com>
2023-03-18 17:25:03 +0900

    SSIA 12.5.3

--- src/main/java/yamsroun/ssiach12/config/SecurityConfig.java
+++ src/main/java/yamsroun/ssiach12/config/SecurityConfig.java
@@ -1,12 +1,22 @@
 package yamsroun.ssiach12.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    @Value("${spring.security.oauth2.client.registration.custom.client-id}")
+    private String clientId;
+
+    @Value("${spring.security.oauth2.client.registration.custom.client-secret}")
+    private String clientSecret;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -15,5 +25,29 @@ public class SecurityConfig {
         http.authorizeHttpRequests()
             .anyRequest().authenticated();
         return http.build();
+    }
+
+    //첫 번째 방법
+    private ClientRegistration clientRegistration1() {
+        return ClientRegistration.withRegistrationId("github")
+            .clientId(clientId)
+            .clientSecret(clientSecret)
+            .scope("read:user")
+            .authorizationUri("https://github.com/login/oauth/authorize")
+            .tokenUri("https://github.com/login/oauth/access_token")
+            .userInfoUri("https://api.github.com/user")
+            .userNameAttributeName("id")
+            .clientName("GibHub")
+            .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+            .build();
+    }
+
+    //두 번째 방법
+    private ClientRegistration clientRegistration2() {
+        return CommonOAuth2Provider.GITHUB
+            .getBuilder("github")
+            .clientId(clientId)
+            .clientSecret(clientSecret)
+            .build();
     }
 }
