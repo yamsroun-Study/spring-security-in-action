6afac16f9d27e0a1acffbd727f31cb6dc3ea6afc
yamsroun <yamsroun@gmail.com>
2023-03-23 23:07:23 +0900

    SSIA 13.3

---
+++ ssia-13-auth-type-password/src/main/java/yamsroun/ssia13/Ssia13Application.java
@@ -0,0 +1,13 @@
+package yamsroun.ssia13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ssia13Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ssia13Application.class, args);
+    }
+
+}
---
+++ ssia-13-auth-type-password/src/test/java/yamsroun/ssia13/Ssia13ApplicationTests.java
@@ -0,0 +1,13 @@
+package yamsroun.ssia13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ssia13ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
