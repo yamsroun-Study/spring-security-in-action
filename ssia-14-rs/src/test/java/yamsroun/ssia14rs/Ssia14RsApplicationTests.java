2b6449f7377b776cba07e266a0557895efd0096e
yamsroun <yamsroun@gmail.com>
2023-06-11 01:06:30 +0900

    SSIA 14 - 리소스 서버

---
+++ ssia-14-rs/src/test/java/yamsroun/ssia14rs/Ssia14RsApplicationTests.java
@@ -0,0 +1,13 @@
+package yamsroun.ssia14rs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ssia14RsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
