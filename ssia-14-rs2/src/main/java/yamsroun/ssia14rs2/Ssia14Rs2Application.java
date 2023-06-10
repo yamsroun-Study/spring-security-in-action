fc8ad024f5e51924d327aca43d9da160b42ef4a2
yamsroun <yamsroun@gmail.com>
2023-06-11 02:26:19 +0900

    SSIA 14 - 리소스 서버 (직접 구성)

---
+++ ssia-14-rs2/src/main/java/yamsroun/ssia14rs2/Ssia14Rs2Application.java
@@ -0,0 +1,13 @@
+package yamsroun.ssia14rs2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ssia14Rs2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ssia14Rs2Application.class, args);
+    }
+
+}
---
+++ ssia-14-rs2/src/test/java/yamsroun/ssia14rs2/Ssia14Rs2ApplicationTests.java
@@ -0,0 +1,13 @@
+package yamsroun.ssia14rs2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ssia14Rs2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
