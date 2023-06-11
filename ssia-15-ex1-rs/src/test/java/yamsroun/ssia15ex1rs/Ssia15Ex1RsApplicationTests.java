fed70ecf3bc6be875584ab8a77feb660b056a7b8
yamsroun <yamsroun@gmail.com>
2023-06-11 23:27:48 +0900

    SSIA 15 - 리소스 서버 (JWT 대칭 키)

---
+++ ssia-15-ex1-rs/src/test/java/yamsroun/ssia15ex1rs/Ssia15Ex1RsApplicationTests.java
@@ -0,0 +1,13 @@
+package yamsroun.ssia15ex1rs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ssia15Ex1RsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
