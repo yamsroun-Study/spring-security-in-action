37766bfd97d70fe382458fddd16a56f19034346d
yamsroun <yamsroun@gmail.com>
2023-06-11 23:49:30 +0900

    SSIA 15 - 리소스 서버 (JWT 대칭 키 - 직접 구성)

---
+++ ssia-15-ex2-rs/src/test/java/yamsroun/ssia15ex2rs/Ssia15Ex2RsApplicationTests.java
@@ -0,0 +1,13 @@
+package yamsroun.ssia15ex2rs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ssia15Ex2RsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
