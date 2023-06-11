01e724200d954855cc72b1d97af6fa6170d3138b
yamsroun <yamsroun@gmail.com>
2023-06-11 20:34:29 +0900

    SSIA 14 - 리소스 서버 (DB)

---
+++ ssia-14-rs-db/src/main/java/yamsroun/ssia14asdb/Ssia14RsDbApplication.java
@@ -0,0 +1,13 @@
+package yamsroun.ssia14asdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ssia14RsDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ssia14RsDbApplication.class, args);
+    }
+
+}
---
+++ ssia-14-rs-db/src/main/java/yamsroun/ssia14asdb/config/ResourceServerConfig.java
@@ -0,0 +1,30 @@
+package yamsroun.ssia14asdb.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableResourceServer
+@RequiredArgsConstructor
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    private final DataSource dataSource;
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.tokenStore(tokenStore());
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new JdbcTokenStore(dataSource);
+    }
+}
