2304cf8ce0a5dd252fd6f1a774b953ba2f2ca6fd
yamsroun <yamsroun@gmail.com>
2023-06-11 20:25:14 +0900

    SSIA 14 - 권한 부여 서버 (DB)

---
+++ ssia-14-as-db/src/test/java/yamsroun/ssia14asdb/Ssia14AsDbApplicationTests.java
@@ -0,0 +1,13 @@
+package yamsroun.ssia14asdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ssia14AsDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
