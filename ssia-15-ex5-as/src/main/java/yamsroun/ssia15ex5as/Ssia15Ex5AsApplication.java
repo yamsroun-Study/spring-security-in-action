f1e20aca92e6871fd41444b788506f62cd49d6eb
yamsroun <yamsroun@gmail.com>
2023-06-17 18:32:29 +0900

    SSIA 15 - 권한 부여 서버 (공개 키 노출 엔드포인트 이용)

---
+++ ssia-15-ex5-as/src/main/java/yamsroun/ssia15ex5as/Ssia15Ex5AsApplication.java
@@ -0,0 +1,13 @@
+package yamsroun.ssia15ex5as;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ssia15Ex5AsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ssia15Ex5AsApplication.class, args);
+    }
+
+}
---
+++ ssia-15-ex5-as/src/test/java/yamsroun/ssia15ex5as/Ssia15Ex5AsApplicationTests.java
@@ -0,0 +1,13 @@
+package yamsroun.ssia15ex5as;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ssia15Ex5AsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
