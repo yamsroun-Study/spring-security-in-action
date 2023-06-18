0dcc70f6235910973106904941ddc425e24d4e05
yamsroun <yamsroun@gmail.com>
2023-06-18 22:48:46 +0900

    SSIA 15 - 리소스 서버 (JWT 맞춤형 세부 정보를 읽을 수 있게 리소스 서버 구성)

---
+++ ssia-15-ex6-rs/src/main/java/yamsroun/ssia15ex6rs/Ssia15Ex6RsApplication.java
@@ -0,0 +1,13 @@
+package yamsroun.ssia15ex6rs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ssia15Ex6RsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ssia15Ex6RsApplication.class, args);
+    }
+
+}
---
+++ ssia-15-ex6-rs/src/main/java/yamsroun/ssia15ex6rs/controller/HelloController.java
@@ -0,0 +1,19 @@
+package yamsroun.ssia15ex6rs.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(OAuth2Authentication authentication) {
+        log.info("hello");
+        var details = (OAuth2AuthenticationDetails) authentication.getDetails();
+        return "Hello! " + details.getDecodedDetails();
+    }
+}
